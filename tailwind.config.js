92c628a827f37459c9f551c1889705b042e995f0
TheJackForge <kimfidler@gmail.com>
2020-11-30 12:45:52 -0700

    Initial Commit of JSOnboarding

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto']
+      },
+    },
+  },
+  variants: {
+    extend: {
+      margin: ['hover'],
+      padding: ['hover']
+    },
+  },
+  plugins: [],
+}
