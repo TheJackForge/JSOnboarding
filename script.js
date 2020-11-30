31eda437896af924d5dffed279c53e33920129cc
TheJackForge <kimfidler@gmail.com>
2020-11-30 16:20:09 -0700

    Event Listeners / JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const conceptsBtn = document.getElementById('concepts-btn')
 const conceptUl = document.getElementById('concept-list')
+const main = document.getElementById('main')
 
 const conceptArray = [
     {
@@ -25,16 +26,52 @@ function conceptOpen() {
 }
 
 function conceptList() {
-    if (conceptUl.innerText === '') {
-    conceptArray.forEach( concept => {
-    const li = document.createElement('li');
-    li.className = ('border mr-4 ml-8 mb-1 pr-4 cursor-pointer transform hover:border-purple-500 hover:bg-purple-500 hover:mr-2 transform hover:pr-6 duration-500')
-    li.innerHTML = `
-        <div id="${concept.id}">${concept.title}</div>
-    `
-    conceptUl.appendChild(li);
-})
+        if (conceptUl.innerText === '') {
+        conceptArray.forEach( concept => {
+        const li = document.createElement('li');
+        li.className = ('border mr-4 ml-8 mb-1 pr-4 cursor-pointer transform hover:border-purple-500 hover:bg-purple-500 hover:mr-2 transform hover:pr-6 duration-500')
+        li.innerHTML = `
+            <div id="${concept.id}">${concept.title}</div>
+        `
+        conceptUl.appendChild(li);
+    })
     }
+}
+
+function assignVariables() {
+    main.innerHTML = '';
+    const assignVariableDiv = document.createElement('div')
+    assignVariableDiv.className = ""
+    assignVariableDiv.innerHTML = `
+    <h1 class="text-4xl">Assigning Variables</h1>
+    <h4 class="mt-4 mb-4 text-base">These are your children...</h4>
+    <div class="border-b-2 mb-5">
+    `
+    main.appendChild(assignVariableDiv)
+}
+
+function eventListen() {
+    main.innerHTML = '';
+    const eventListenDiv = document.createElement('div')
+    eventListenDiv.className = ""
+    eventListenDiv.innerHTML = `
+    <h1 class="text-4xl">Event Listeners</h1>
+    <h4 class="mt-4 mb-4 text-base">The corporation is always listening...</h4>
+    <div class="border-b-2 mb-5">
+    `
+    main.appendChild(eventListenDiv)
+}
+
+function functionPage() {
+    main.innerHTML = '';
+    const functionPageDiv = document.createElement('div')
+    functionPageDiv.className = ""
+    functionPageDiv.innerHTML = `
+    <h1 class="text-4xl">Functions</h1>
+    <h4 class="mt-4 mb-4 text-base">What purpose do you serve?</h4>
+    <div class="border-b-2 mb-5">
+    `
+    main.appendChild(functionPageDiv)
 }
 
 conceptsBtn.addEventListener('click', conceptOpen)
@@ -42,6 +79,14 @@ conceptsBtn.addEventListener('click', conceptOpen)
 conceptUl.addEventListener('click', (e)=> {
     console.log(e.target)
     if (e.target.id === 'assign-variables') {
-        console.log('ASSIGN VARIABLES')
+        assignVariables();
+    }
+
+    if (e.target.id === 'event-listeners') {
+        eventListen();
+    }
+
+    if (e.target.id === 'functions') {
+        functionPage();
     }
 })
