f63ac58695f430c225b756678c6627dba06bd98a
thejackforge <kimfidler@gmail.com>
2020-12-01 12:23:22 -0700

    Obtaining Elements Started

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const conceptArray = [
         'id': 'assign-variables' 
     },
     {
-        'title': 'Selectors',
-        'id': 'selectors'
+        'title': 'Obtaining Elements',
+        'id': 'obtainingElements'
     },
     {
         'title': 'Event Listeners',
@@ -44,9 +44,9 @@ function conceptList() {
 
 function assignVariables() {
     main.innerHTML = '';
-    const assignVariableDiv = document.createElement('div')
-    assignVariableDiv.className = "mb-20"
-    assignVariableDiv.innerHTML = `
+    const operatorDiv = document.createElement('div')
+    operatorDiv.className = "mb-20"
+    operatorDiv.innerHTML = `
     <h1 class="text-4xl">Declaring Variables</h1>
     <h4 class="mt-4 mb-4 text-base">These are your children...</h4>
     <div class="border-b-2 mb-5"></div>
@@ -82,7 +82,65 @@ function assignVariables() {
     <button id="const2Btn" class="text-aqua text-xs items-center bg-black pl-1 pr-1 mt-2 mb-4 z-10 focus:outline-none">
     PROCESS ANSWER</button><div id="const2Result"></div>
     `
-    main.appendChild(assignVariableDiv)
+    main.appendChild(operatorDiv)
+}
+
+function obtainingElements() {
+    main.innerHTML = '';
+    const operatorDiv = document.createElement('div')
+    operatorDiv.className = "mb-20"
+    operatorDiv.innerHTML = `
+    <h1 class="text-4xl">Obtaining Elements</h1>
+    <h4 class="mt-4 mb-4 text-base">Sometimes you just need to take the things the corporation needs...</h4>
+    <div class="border-b-2 mb-5"></div>
+    <p class="mb-5">drone euro-pop render-farm garage DIY nodality augmented reality. modem Shibuya office rifle rebar convenience store sensory. pre- render-farm hacker human dome plastic katana. into papier-mache BASE jump modem math- neural 8-bit. free-market hacker refrigerator warehouse convenience store euro-pop j-pop. savant bicycle fluidity rifle range-rover narrative sprawl. receding neural receding ablative drone tattoo savant. drone stimulate kanji post- augmented reality pre- Shibuya. faded vinyl rain range-rover neon computer drone.</p>
+    <p class="mb-5">Below are some of the methods we can employ to get the information that we need:</p>
+    
+    <p class="border-b border-r border-pink-500 inline-block pr-4">METHOD getElementById</li>
+    
+    <p class="mb-2">Method <span class="text-purple-500">getElementById</span> is what we employ the most at the corporation. This will allow you to attach onto an element that has a defined ID. </p>
+    <p class="bg-black mb-5 pl-2">const cyberElement = document.<span class="text-lime">getElementById</span>('cyberElementId')</p>
+
+    <p class="border-b border-r border-pink-500 inline-block pr-4">METHOD querySelector</p>
+
+    <p class="mb-2">Method <span class="text-purple-500">querySelector</span> allows the operator to attach onto an element that has a defined class. This is necessary to use when you are doing a scan but the elements' ID is not identified.</p>
+
+    <p class="bg-black mb-5 pl-2">const cyberElement = document.<span class="text-lime">querySelector</span>('.cyberElementClass')</p>
+    
+    <p class="border-b border-r border-pink-500 inline-block pr-4">METHOD querySelectorAll</p>
+
+    <p class="mb-2">Method <span class="text-purple-500">querySelectorAll</span> allows you to grab multiple class elements at once. This will be used when you need to employ certain functions and tactics to a group.</p>
+
+    <p class="bg-black mb-5 pl-2">const cyberElements = document.<span class="text-lime">querySelectorAll</span>('.cyberElementGroup')</p>
+
+    <p class="border-b border-r border-pink-500 inline-block pr-4">METHOD getElementsByTagName</li>
+    
+    <p class="mb-5">Method <span class="text-purple-500">getElementsByTagName</span> attaches to an HTMLCollection of elements. For example, If you would like to grab all of the 'h1' elements, you would use this method.</p>
+
+    <p class="bg-black mb-5 pl-2">const cyberHeadings = document.<span class="text-lime">getElementsByTagName</span>('h1')</p>
+    
+    <small>Example of Declaring Variables</small>
+
+    <div class="bg-black text-xs pl-2 pt-2 pb-2 mb-5">
+    <p><span class="text-purple-500">const</span> insertionTeam1 = 5;</p> 
+    <p class="mb-2">// The number of members on this insertion team <span class="text-lime">CANNOT</span> be changed at any point in time.</p>
+
+    <p><span class="text-purple-500">let</span> insertionTeam2 = 5;</p>
+    <p class="mb-2">// The number of members on this insertion team <span class="text-lime">CAN</span> be changed at any point in time.</p>
+
+    <p class>If at any point in the mission you change the amount of members on insertionTeam1, the <span class="text-purple-500">const</span> declaration will cause the system to output an error. However, because of the <span class="text-purple-500">let</span> declaration you can change the number of members on insertionTeam2 at anytime.</p>
+    </div>
+
+    <small>Please answer the following questions</small>
+    <p>1. If you are tasked with operating a top secret mission, and are forbidden from bringing in any extra operatives. You would need to use the <input id="const1" class="bg-black text-aqua mr-1 ml-1 h-5 w-20 text-center"></input>declaration. </p>
+    <button id="const1Btn" class="text-aqua text-xs items-center bg-black pl-1 pr-1 mt-2 mb-4 z-10 focus:outline-none">
+    PROCESS ANSWER</button><div id="const1Result" class=""></div>
+
+    <p>2. While monitoring your teams vital stats, you notice that their health begins to deteriorate. You feel that sending in a Stim-Operative would be beneficial to the team. To increase the number of members on the team, you would need to use the <input id="const2" class="bg-black text-aqua mr-1 ml-1 h-5 w-20 text-center"></input> declaration.</p>
+    <button id="const2Btn" class="text-aqua text-xs items-center bg-black pl-1 pr-1 mt-2 mb-4 z-10 focus:outline-none">
+    PROCESS ANSWER</button><div id="const2Result"></div>
+    `
+    main.appendChild(operatorDiv)
 }
 
 function eventListen() {
@@ -115,6 +173,10 @@ conceptUl.addEventListener('click', (e)=> {
     console.log(e.target)
     if (e.target.id === 'assign-variables') {
         assignVariables();
+    }
+
+    if (e.target.id === 'obtainingElements') {
+        obtainingElements();
     }
 
     if (e.target.id === 'event-listeners') {
@@ -157,3 +219,5 @@ main.addEventListener('click', (e) => {
         }
     }
 })
+
+// Obtaining DOM Elements Questions
