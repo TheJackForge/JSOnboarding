8912d962552defc1b061c150d7f0f94b7953feb8
thejackforge <kimfidler@gmail.com>
2020-12-01 14:08:48 -0700

    Obtaining Elements Work on Questions

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function assignVariables() {
     
     <small>Example of Declaring Variables</small>
 
-    <div class="bg-black text-xs pl-2 pt-2 pb-2 mb-5">
+    <div class="bg-black text-s pl-2 pt-2 pb-2 mb-5">
     <p><span class="text-purple-500">const</span> insertionTeam1 = 5;</p> 
     <p class="mb-2">// The number of members on this insertion team <span class="text-lime">CANNOT</span> be changed at any point in time.</p>
 
@@ -119,26 +119,24 @@ function obtainingElements() {
 
     <p class="bg-black mb-5 pl-2">const cyberHeadings = document.<span class="text-lime">getElementsByTagName</span>('h1')</p>
     
-    <small>Example of Declaring Variables</small>
+    <p>Simulation Exercises</p>
 
-    <div class="bg-black text-xs pl-2 pt-2 pb-2 mb-5">
-    <p><span class="text-purple-500">const</span> insertionTeam1 = 5;</p> 
-    <p class="mb-2">// The number of members on this insertion team <span class="text-lime">CANNOT</span> be changed at any point in time.</p>
-
-    <p><span class="text-purple-500">let</span> insertionTeam2 = 5;</p>
-    <p class="mb-2">// The number of members on this insertion team <span class="text-lime">CAN</span> be changed at any point in time.</p>
-
-    <p class>If at any point in the mission you change the amount of members on insertionTeam1, the <span class="text-purple-500">const</span> declaration will cause the system to output an error. However, because of the <span class="text-purple-500">let</span> declaration you can change the number of members on insertionTeam2 at anytime.</p>
+    <div class="bg-black text-s pl-2 pt-2 pb-2 mb-5">
+    <p class="mb-2">On a mission you come across a tablet that you must break into. In order to do this you need to pull out several elements to make changes to. You are presented with the following code when you penetrate the initial layer of the firewall:</p> 
+    <p>Device INITIATED AT 00:29 // Alarms have not been triggered</p>
+    <p>Results have come back NEGATIVE</p>
+    <p>KEY Elements are in Resting State // P4623</p>
+    <p>EMPLOYEE CLASS === CEO STATUS // REDACTED (ID NUMBER = &lt;div id="ceoStatus"&gt;)
     </div>
 
     <small>Please answer the following questions</small>
-    <p>1. If you are tasked with operating a top secret mission, and are forbidden from bringing in any extra operatives. You would need to use the <input id="const1" class="bg-black text-aqua mr-1 ml-1 h-5 w-20 text-center"></input>declaration. </p>
-    <button id="const1Btn" class="text-aqua text-xs items-center bg-black pl-1 pr-1 mt-2 mb-4 z-10 focus:outline-none">
-    PROCESS ANSWER</button><div id="const1Result" class=""></div>
+    <p>1. What phrase is the CEO ID linked to? <input id="obtainingElements1" class="bg-black text-aqua mr-1 ml-1 h-5 w-20 text-center"></input></p>
+    <button id="obtainingElements1Btn" class="text-aqua text-xs items-center bg-black pl-1 pr-1 mt-2 mb-4 z-10 focus:outline-none">
+    PROCESS ANSWER</button><div id="obtainingElements1Result" class=""></div>
 
-    <p>2. While monitoring your teams vital stats, you notice that their health begins to deteriorate. You feel that sending in a Stim-Operative would be beneficial to the team. To increase the number of members on the team, you would need to use the <input id="const2" class="bg-black text-aqua mr-1 ml-1 h-5 w-20 text-center"></input> declaration.</p>
-    <button id="const2Btn" class="text-aqua text-xs items-center bg-black pl-1 pr-1 mt-2 mb-4 z-10 focus:outline-none">
-    PROCESS ANSWER</button><div id="const2Result"></div>
+    <p>2. To obtain <span id="redacted2" class="bg-black text-red-500">REDACTED</span> ID we would need to use the <input id="obtainingElements2" class="bg-black text-aqua mr-1 ml-1 h-5 w-30 text-center"></input> method.</p>
+    <button id="obtainingElements2Btn" class="text-aqua text-xs items-center bg-black pl-1 pr-1 mt-2 mb-4 z-10 focus:outline-none">
+    PROCESS ANSWER</button><div id="obtainingElements2Result"></div>
     `
     main.appendChild(operatorDiv)
 }
@@ -221,3 +219,40 @@ main.addEventListener('click', (e) => {
 })
 
 // Obtaining DOM Elements Questions
+
+main.addEventListener('click', (e) => {
+    const obtainingElements1 = document.getElementById('obtainingElements1')
+    const obtainingElements1Result = document.getElementById('obtainingElements1Result')
+    const obtainingElements2Redacted = document.getElementById('redacted2')
+    if (e.target.id === 'obtainingElements1Btn') {
+        // const const1Btn = document.getElementById('const1Btn')
+        if (obtainingElements1.value === 'ceoStatus') {
+            obtainingElements1Result.innerHTML = `<p>CORRECT</p>`
+            obtainingElements1Result.className = 'text-lime bg-black pl-10 pr-2 inline-block text-xs'
+            obtainingElements2Redacted.classList.remove('text-red-500')
+            obtainingElements2Redacted.classList.add('text-lime')
+            obtainingElements2Redacted.innerText = 'UNLOCKED'
+            setTimeout(function(){ 
+                obtainingElements2Redacted.innerText = 'ceoStatus'; 
+            }, 2000);
+        } else {
+            obtainingElements1Result.innerHTML = `<p>INCORRECT</p>`
+            obtainingElements1Result.className = 'text-red-500 bg-black pl-10 pr-2 inline-block text-xs'
+        }
+    }
+})
+
+main.addEventListener('click', (e) => {
+    const obtainingElements1 = document.getElementById('obtainingElements2')
+    const obtainingElements1Result = document.getElementById('obtainingElements2Result')
+    if (e.target.id === 'obtainingElements2Btn') {
+        // const const1Btn = document.getElementById('const1Btn')
+        if (obtainingElements1.value === 'getElementById') {
+            obtainingElements1Result.innerHTML = `<p>CORRECT</p>`
+            obtainingElements1Result.className = 'text-lime bg-black pl-10 pr-2 inline-block text-xs'
+        } else {
+            obtainingElements1Result.innerHTML = `<p>INCORRECT</p>`
+            obtainingElements1Result.className = 'text-red-500 bg-black pl-10 pr-2 inline-block text-xs'
+        }
+    }
+})
